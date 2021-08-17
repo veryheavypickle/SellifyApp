def43fab564752be553294467c5344a7e7189593
veryheavypickle <xavier.carroll01@gmail.com>
2021-08-17 13:30:34 +0100

    Initial Commit

---
+++ Sellify/SellifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SellifyApp.swift
+//  Sellify
+//
+//  Created by Xavier Carroll on 17/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SellifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
