56292499628b974f2d90145ad5baf7da55904897
xaviercarroll <xavier.carroll01@gmail.com>
2021-08-17 13:30:34 +0100

    Initial Commit

---
+++ Sellify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sellify
+//
+//  Created by Xavier Carroll on 17/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
